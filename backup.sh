5eedee85cd53df457ef64a0a94bce31fd2ab7fb0
Gary Verhaegen <gary.verhaegen@gmail.com>
2024-04-07 12:26:16 +0200

    include private repos

--- backup.sh
+++ backup.sh
@@ -4,11 +4,22 @@ set -euo pipefail
 
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 
+if [ -z "${GITHUB_TOKEN:-}" ]; then
+  echo "This script requires a GitHub token in the GITHUB_TOKEN env var."
+  echo "You can set it by adding the following line to .envrc.private:"
+  echo
+  echo "export GITHUB_TOKEN=<your token>"
+  echo
+  echo "The token should have metadata read access to all the repositories you"
+  echo "want to backup."
+  exit 1
+fi
+
 TODAY=$(date -I)
 
 if [ -d "$DIR"/_backups/$TODAY.tar.gz ]; then
-    echo "Backup already done?"
-    exit 1
+  echo "Backup already done today."
+  exit 1
 fi
 
 tmp=$(mktemp -d)
@@ -17,10 +28,20 @@ trap "rm -rf $tmp" EXIT
 mkdir $tmp/$TODAY
 cd $tmp/$TODAY
 
-REPOS=$(curl -s 'https://api.github.com/users/gaverhae/repos' | jq -r '.[].ssh_url')
+if [ -n "${GITHUB_TOKEN:-}" ]; then
+  auth="-H \"Authorization: Bearer $GITHUB_TOKEN\""
+else
+  auth=
+fi
+
+REPOS=$(curl --silent \
+             -H "Authorization: Bearer $GITHUB_TOKEN" \
+             https://api.github.com/user/repos \
+        | jq -r '.[].ssh_url')
 
 for r in $REPOS; do
-    git clone --bare $r
+  echo $r
+  git clone --bare $r
 done
 
 cd ..
