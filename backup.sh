60bbd8daee06477048cc944aa9adae3a18b97463
Gary Verhaegen <gary.verhaegen@gmail.com>
2024-04-07 12:11:28 +0200

    put backups in subfolder

--- backup.sh
+++ backup.sh
@@ -3,17 +3,19 @@
 set -euo pipefail
 
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
-cd $DIR
 
 TODAY=$(date -I)
 
-if [ -d $TODAY ]; then
+if [ -d "$DIR"/_backups/$TODAY.tar.gz ]; then
     echo "Backup already done?"
     exit 1
 fi
 
-mkdir $TODAY
-cd $TODAY
+tmp=$(mktemp -d)
+trap "rm -rf $tmp" EXIT
+
+mkdir $tmp/$TODAY
+cd $tmp/$TODAY
 
 REPOS=$(curl -s 'https://api.github.com/users/gaverhae/repos' | jq -r '.[].ssh_url')
 
@@ -24,4 +26,4 @@ done
 cd ..
 
 GZIP=-9 tar czf $TODAY.tar.gz $TODAY
-rm -rf $TODAY
+mv $TODAY.tar.gz "$DIR"/_backups/
