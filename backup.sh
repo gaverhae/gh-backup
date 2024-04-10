f8655522cfd5373000a1a90f978ed77dff3d5225
Gary Verhaegen <gary.verhaegen@gmail.com>
2024-04-10 19:20:39 +0200

    deal with GitHub pagination (#4)

--- backup.sh
+++ backup.sh
@@ -28,10 +28,30 @@ trap "rm -rf $tmp" EXIT
 mkdir $tmp/$TODAY
 cd $tmp/$TODAY
 
-REPOS=$(curl --silent \
-             -H "Authorization: Bearer $GITHUB_TOKEN" \
-             https://api.github.com/user/repos \
-        | jq -r '.[].ssh_url')
+get_repos() (
+  url=https://api.github.com/user/repos
+  auth="Authorization: Bearer $GITHUB_TOKEN"
+  tmp_dir=$(mktemp -d)
+  trap "rm -rf $tmp_dir" EXIT
+  while [ -n "$url" ]; do
+    curl $url \
+         -H "$auth" \
+         -H "Accept: application/vnd.github+json" \
+         -H "X-GitHub-Api-Version: 2022-11-28" \
+         --silent \
+         --fail \
+         -o >(jq -r '.[].ssh_url' >> $tmp_dir/resp) \
+         -D $tmp_dir/headers
+    url=$(cat $tmp_dir/headers \
+           | tr -d '\r' \
+           | grep "link:" \
+           | grep -Po '(?<=<)([^>]*)(?=>; rel="next")' \
+           || true)
+  done
+  cat $tmp_dir/resp
+)
+
+REPOS=$(get_repos)
 
 log=$(mktemp)
 for r in $REPOS; do
